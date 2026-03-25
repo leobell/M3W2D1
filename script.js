17047aef99b3e166d26f922ed26359b683d8cd9d
leobell <bellleonardo04@gmail.com>
2026-03-25 13:43:57 +0100

    es-W2-D4

---
+++ script.js
@@ -0,0 +1,50 @@
+const apiKey = "D1BQRs4yer11JQeoGDBvP7vxPvB9jWur9ncBVwoqRYWlgkZYrbJXikyB"
+
+const searchBtn = document.querySelector(".buttonSearch")
+const searchInput = document.querySelector(".inputSearch")
+
+
+
+const getPhotos = async () => {
+    try{
+        const rawData = await fetch (`https://api.pexels.com/v1/search?query=${searchInput.value}`,{
+            headers: {
+                "Authorization": apiKey
+            }
+        })
+
+        const data = await rawData.json()
+
+        return data
+            
+    } catch(error){
+        console.log(error)
+    }
+}
+
+const generateCard = (photo) =>{
+    const cardContainer = document.createElement("div")
+    cardContainer.classList.add("card-container")
+    //cardContainer.classList.add("card")
+
+    const img = document.createElement("img")
+    img.src=photo.src.original
+    img.classList.add("card-img-top")
+
+    const artist = document.createElement("h3")
+    artist.innerText= photo.photographer
+
+    const cards = document.querySelector(".cards")
+    cardContainer.append(img,artist)
+    cards.appendChild(cardContainer)
+}
+
+searchBtn.addEventListener("click", async () => {
+    const cards = document.querySelector(".cards")
+    cards.innerHTML=""
+    const data = await getPhotos()
+    console.log(data)
+    data.photos.forEach(photo => {
+        generateCard(photo)
+    });
+})
